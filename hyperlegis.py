8d2b56f4b60559b2ca753481c040bc207c37d479
Forrest Murray <FMurray@gmail.com>
2023-06-23 14:10:13 -0400

    add super_dataframe fun

--- hyperlegis.py
+++ hyperlegis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import hyperdemocracy as hd
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from super_dataframe import super_dataframe
 
 
 st.title('Hyperlegis - Ask Questions About Legislation')
@@ -25,19 +26,23 @@ def search_filter(df, search_term):
     df[df.apply(lambda row: search_term.lower() in row.astype(str).str.lower().any(), axis=1)]
 
 data = _load_assembleco_records()
+editable_df = st.data_editor(super_dataframe(data))
 
-search_query = st.text_input("Search for legislation by keyword")
-if st.button('Search') and search_query != '':
-    data = hd.filter_aco_df(data, search_query)
+# def get_editable_df(df):
+#     return st.data_editor(df,
+#     column_config={
+#         "congress_gov_url": st.column_config.LinkColumn("Congress.gov URL"),
+#     },
+#     hide_index=True
+# )
 
-st.subheader('Assembleco Records 📜')
-edited_df = st.data_editor(data,
-    column_config={
-        "congress_gov_url": st.column_config.LinkColumn("Congress.gov URL"),
-    },
-    hide_index=True
-)
-st.caption('use ⌘ Cmd + F or Ctrl + F to search the table')
+# search_query = st.text_input("Search for legislation by keyword")
+# if st.button('Search') and search_query != '':
+#     editable_df = hd.filter_aco_df(data, search_query)
+
+# st.subheader('Assembleco Records 📜')
+# editable_df = get_editable_df(data)
+# st.caption('use ⌘ Cmd + F or Ctrl + F to search the table')
 
 def langchain_setup(n_docs=100): 
     docs = hd.get_legislative_documents_from_df(data)
---
+++ super_dataframe.py
@@ -0,0 +1,91 @@
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+import pandas as pd
+import streamlit as st
+from hyperdemocracy import filter_aco_df
+
+def super_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Filter or Search")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter by", df.columns)
+        search_input = st.text_input("Search for", key="text")
+
+        if search_input is not "":
+            df = filter_aco_df(df, search_input)
+        else: 
+            df = df
+
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Search {column} for:",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
